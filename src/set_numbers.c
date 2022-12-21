621fc348c57c0338a7bc448dd7f1e77dba9f20ef
gsmereka <gabrielsmereka200@gmail.com>
2022-12-21 16:42:51 -0300

    Set Numbers reworked

--- src/set_numbers.c
+++ src/set_numbers.c
@@ -6,15 +6,16 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 22:57:58 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/17 19:12:17 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/21 16:28:37 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/push_swap.h"
 
-static int	arg_to_int(char *arg, t_data *data);
-static int	validate_number(int nbr, char *original_nbr, t_data *data);
+static int	arg_to_integer(char *arg, t_data *data);
+static int	check_integer_size(int nbr, char *original_nbr, t_data *data);
 static int	compare_absolute_values(char *nbr_copy, char *original_nbr);
+static int	check_integer_duplicates(int *int_list, int int_pos, t_data *data);
 
 void	set_numbers(char **argv, t_data *data)
 {
@@ -23,34 +24,34 @@ void	set_numbers(char **argv, t_data *data)
 	i = 0;
 	while (argv[i + 1])
 	{
-		data->user_args[i] = arg_to_int(argv[i + 1], data);
-		validate_number(data->user_args[i], argv[i + 1], data);
+		data->user_args[i] = arg_to_integer(argv[i + 1], data);
+		check_integer_size(data->user_args[i], argv[i + 1], data);
+		check_integer_duplicates(data->user_args, i, data);
 		i++;
 	}
 }
 
-static int	arg_to_int(char *arg, t_data *data)
+static int	arg_to_integer(char *arg, t_data *data)
 {
 	int	sign;
 	int	save;
 	int	i;
 
 	i = 0;
+	while ((arg[i] >= '\t' && arg[i] <= '\r') || arg[i] == ' ')
+		i++;
 	save = 0;
 	sign = 1;
 	if (arg[i] == '-')
 		sign = -1;
-	if (arg[i] == '+' || arg[i] == '-' || ft_isdigit(arg[i]) > 0)
+	if (arg[i] == '-' || arg[i] == '+')
+		i++;
+	while (ft_isdigit(arg[i]) > 0)
 	{
-		if (arg[i] == '-' || arg[i] == '+')
-			i++;
-		while (ft_isdigit(arg[i]) > 0)
-		{
-			save = (save * 10) + (arg[i] - '0');
-			i++;
-		}
+		save = (save * 10) + (arg[i] - '0');
+		i++;
 	}
-	if (arg[i] != '\0' && ft_isdigit(arg[i]) == 0)
+	if (arg[i] != '\0')
 	{
 		ft_printf("'%s' is not an integrer\n", arg);
 		exit_error(1, data);
@@ -58,12 +59,14 @@ static int	arg_to_int(char *arg, t_data *data)
 	return (save * sign);
 }
 
-static int	validate_number(int nbr, char *original_nbr, t_data *data)
+static int	check_integer_size(int nbr, char *original_nbr, t_data *data)
 {
 	char	*nbr_copy;
 	int		diff;
 
 	nbr_copy = ft_itoa(nbr);
+	if (!nbr_copy)
+		exit_error(12, data);
 	diff = compare_absolute_values(nbr_copy, original_nbr);
 	free(nbr_copy);
 	if (diff != 0)
@@ -77,18 +80,31 @@ static int	validate_number(int nbr, char *original_nbr, t_data *data)
 static int	compare_absolute_values(char *nbr_copy, char *original_nbr)
 {
 	int	diff;
-	int	ptr_1;
-	int	ptr_2;
 
-	ptr_1 = 0;
-	ptr_2 = 0;
 	if (*nbr_copy == '-')
-		ptr_1++;
+		nbr_copy++;
+	while ((*original_nbr >= '\t' && *original_nbr <= '\r')
+		|| *original_nbr == ' ')
+		original_nbr++;
 	if (*original_nbr == '+' || *original_nbr == '-')
-		ptr_2++;
-	while (*original_nbr + ptr_2 == '0' && *original_nbr + 1 + ptr_2 != '\0')
-		ptr_2++;
-	diff = ft_strncmp(nbr_copy + ptr_1, original_nbr + ptr_2,
-			ft_strlen(original_nbr + ptr_2));
+		original_nbr++;
+	while (*original_nbr == '0' && *original_nbr + 1 != '\0')
+		original_nbr++;
+	diff = ft_strncmp(nbr_copy, original_nbr,
+			ft_strlen(original_nbr));
 	return (diff);
+}
+
+static int	check_integer_duplicates(int *int_list, int int_pos, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < int_pos)
+	{
+		if (int_list[i] == int_list[int_pos])
+			exit_error(1, data);
+		i++;
+	}
+	return (0);
 }
