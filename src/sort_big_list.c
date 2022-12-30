b10cc217c58e48e9938d40f97e5463cbb5e23c25
gsmereka <gabrielsmereka200@gmail.com>
2022-12-30 17:40:50 -0300

    set norminette in sort_big_list

--- src/sort_big_list.c
+++ src/sort_big_list.c
@@ -6,52 +6,27 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 17:32:52 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/30 17:15:11 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:38:17 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/push_swap.h"
 
 static int	max_binary_size(t_data *data);
+static void	split_nodes_by_digit_value(int digit_value, t_data *data);
+static void	merge_stacks(t_data *data);
 
 int	sort_big_list(t_data *data)
 {
-	int	max_size;
 	int	digit_pos;
-	int	less_value_digit_pos;
-	int	i;
+	int	max_size;
 
 	digit_pos = 1;
 	max_size = max_binary_size(data);
 	while (digit_pos < max_size)
 	{
-		i = 0;
-		while (i < data->max_stack_size)
-		{
-			less_value_digit_pos = ft_strlen(data->stack_a->binary_nmb);
-			less_value_digit_pos = less_value_digit_pos - digit_pos;
-			if (less_value_digit_pos < 0)
-			{
-				ft_push('a', data);
-				ft_printf("pa\n");
-			}
-			else if (data->stack_a->binary_nmb[less_value_digit_pos] == '0')
-			{
-				ft_push('a', data);
-				ft_printf("pa\n");
-			}
-			else
-			{
-				ft_rotate('a', data);
-				ft_printf("ra\n");
-			}
-			i++;
-		}
-		while (data->stack_b)
-		{
-			ft_push('b', data);
-			ft_printf("pb\n");
-		}
+		split_nodes_by_digit_value(digit_pos, data);
+		merge_stacks(data);
 		digit_pos++;
 	}
 	return (0);
@@ -73,4 +48,42 @@ static int	max_binary_size(t_data *data)
 	}
 	max_size = ft_strlen(big_node->binary_nmb);
 	return (max_size);
+}
+
+static void	split_nodes_by_digit_value(int digit_pos, t_data *data)
+{
+	int	less_value_digit_pos;
+	int	i;
+
+	i = 0;
+	while (i < data->max_stack_size)
+	{
+		less_value_digit_pos = ft_strlen(data->stack_a->binary_nmb);
+		less_value_digit_pos = less_value_digit_pos - digit_pos;
+		if (less_value_digit_pos < 0)
+		{
+			ft_push('a', data);
+			ft_printf("pa\n");
+		}
+		else if (data->stack_a->binary_nmb[less_value_digit_pos] == '0')
+		{
+			ft_push('a', data);
+			ft_printf("pa\n");
+		}
+		else
+		{
+			ft_rotate('a', data);
+			ft_printf("ra\n");
+		}
+		i++;
+	}
+}
+
+static void	merge_stacks(t_data *data)
+{
+	while (data->stack_b)
+	{
+		ft_push('b', data);
+		ft_printf("pb\n");
+	}
 }
