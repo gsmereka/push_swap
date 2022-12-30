76c63a2fbf30599b9464bb794f60b48d5a2e6740
gsmereka <gabrielsmereka200@gmail.com>
2022-12-30 17:28:55 -0300

    att init_sort

--- src/init_sort.c
+++ src/init_sort.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 22:17:43 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/30 17:02:35 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:28:02 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ static int	stack_a_is_sorted(t_data *data);
 int	init_sort(t_data *data)
 {
 	if (stack_a_is_sorted(data))
-		ft_printf("Ta certa\n");
-	// if (data->max_stack_size <= 5)
-	// 	sort_small_list(data);
-	// if (data->max_stack_size > 5)
-	// {
+		return (0);
+	if (data->max_stack_size <= 5)
+		sort_small_list(data);
+	if (data->max_stack_size > 5)
+	{
 		simplify_numbers(data);
 		set_binary_values(data->stack_a, data);
 		sort_big_list(data);
-	// }
+	}
 	return (0);
 }
 
