f69e53a9cac409c837888df170d9ca85ad79e862
gsmereka <gabrielsmereka200@gmail.com>
2022-12-30 16:09:45 -0300

    att sort_big_list

--- src/sort_big_list.c
+++ src/sort_big_list.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 17:32:52 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/30 15:25:39 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/30 16:07:15 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 int	sort_big_list(t_data *data)
 {
-	int	increment_digit_value;
-	int	less_value_digit;
+	int	digit_pos;
+	int	less_value_digit_pos;
 	int	i;
 
-	increment_digit_value = 1;
-	while (increment_digit_value < 5)
+	digit_pos = 1;
+	while (digit_pos < 7)
 	{
 		i = 0;
 		while (i < data->max_stack_size)
 		{
-			less_value_digit = ft_strlen(data->stack_a->binary_nmb) - increment_digit_value;
-			if (data->stack_a->binary_nmb[less_value_digit] == '0')
+			less_value_digit_pos = ft_strlen(data->stack_a->binary_nmb);
+			less_value_digit_pos = less_value_digit_pos - digit_pos;
+			if (data->stack_a->binary_nmb[less_value_digit_pos] == '0')
 				ft_push('a', data);
 			else
 				ft_rotate('a', data);
@@ -33,7 +34,7 @@ int	sort_big_list(t_data *data)
 		}
 		while (data->stack_b)
 			ft_push('b', data);
-		increment_digit_value++;
+		digit_pos++;
 	}
 	return (0);
 }
