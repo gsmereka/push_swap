719bbb409e4f0e629a098b9ad1049ea20924d0f4
gsmereka <gabrielsmereka200@gmail.com>
2022-12-30 18:00:29 -0300

    set simplify_numbers to all sorts

--- src/init_sort.c
+++ src/init_sort.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 22:17:43 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/30 17:28:02 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:59:58 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ int	init_sort(t_data *data)
 {
 	if (stack_a_is_sorted(data))
 		return (0);
+	simplify_numbers(data);
+	set_binary_values(data->stack_a, data);
 	if (data->max_stack_size <= 5)
 		sort_small_list(data);
 	if (data->max_stack_size > 5)
-	{
-		simplify_numbers(data);
-		set_binary_values(data->stack_a, data);
 		sort_big_list(data);
-	}
 	return (0);
 }
 
