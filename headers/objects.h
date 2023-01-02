f57ecf61d27e8ecd5d9a6b9cae4d327e8b325004
gsmereka <gabrielsmereka200@gmail.com>
2023-01-02 18:29:22 -0300

    add object

--- headers/objects.h
+++ headers/objects.h
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 11:02:22 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/31 16:16:34 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/01/02 18:27:15 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@ typedef struct s_list
 	struct s_list	*next;
 	struct s_list	*prev;
 }	t_list;
+
+typedef struct s_rule
+{
+	char			*instruction;
+	int				stack_a_size;
+	int				stack_b_size;
+	struct s_rule	*next;
+}	t_rule;
 
 typedef struct s_swap
 {
@@ -42,6 +50,7 @@ typedef struct s_data
 	int				*user_args;
 	int				stack_a_size;
 	int				stack_b_size;
+	struct s_rule	*rule;
 	struct s_list	*stack_a;
 	struct s_list	*stack_b;
 }	t_data;
